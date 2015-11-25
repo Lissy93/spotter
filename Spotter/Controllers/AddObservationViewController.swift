68626aba9037e25bb152a8b4781b2a3a18c4cbe3
Alicia Sykes <alicia@aliciasykes.com>
2015-11-25 10:29:51 +0000

    Add observation view, added contrstrains and linked UI to code

---
+++ Spotter/Controllers/AddObservationViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddObservationViewController.swift
+//  Spotter
+//
+//  Created by Alicia Sykes on 25/11/2015.
+//  Copyright © 2015 Alicia Sykes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddObservationViewController : UIViewController{
+
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtObservationName: UITextField!
+    @IBOutlet weak var txtDescription: UITextField!
+    @IBOutlet weak var txtLatitude: UITextField!
+    @IBOutlet weak var txtLongitude: UITextField!
+    @IBOutlet weak var txtCategory: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    
+    @IBAction func savePressed(sender: UIButton) {
+        
+        
+    }
+    
+
+    
+    
+
+
+}

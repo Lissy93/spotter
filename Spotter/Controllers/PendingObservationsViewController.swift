4dfb9c9a5e1a297c1f36258efac53f6804d26e22
Alicia Sykes <alicia@aliciasykes.com>
2015-12-07 09:50:23 +0000

    linked pending observations with VC

---
+++ Spotter/Controllers/PendingObservationsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PendingObservationsViewController.swift
+//  Spotter
+//
+//  Created by Alicia Sykes on 07/12/2015.
+//  Copyright © 2015 Alicia Sykes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PendingObservationsViewController : UIViewController{
+
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func uploadObservationsPressed(sender: UIButton) {
+        
+        
+    }
+
+
+}
